aaa79fafd978cd20481b9edbe6c81ba5c1d24bfc
Sebastian Jurewicz <senkartan@gmail.com>
2022-09-20 12:15:47 +0200

    Task 2 Done

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const numberOfCategories = Array.from(
+  document.querySelectorAll(".item")
+).length;
+
+console.log(`Number of categories: ${numberOfCategories}`);
+
+//----------------------------------
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.getElementById('ingredients');
+
+for (let ingredient of ingredients) {
+  const ingredientElement = document.createElement('li');
+  ingredientElement.innerText = ingredient;
+  ingredientElement.classList.add('item');
+  ingredientsList.appendChild(ingredientElement);
+}
