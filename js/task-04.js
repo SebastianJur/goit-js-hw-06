445b9993b74a30b6b8df624b49a18cf101dd2f7f
Sebastian Jurewicz <senkartan@gmail.com>
2022-09-22 13:37:16 +0200

    Task Done 4

---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+
+const counter = document.querySelector("#value");
+let counterValue = 0;
+
+function Minus(event) {
+  counterValue -= 1;
+  return (counter.innerHTML = counterValue);
+}
+
+function Plus(event) {
+  counterValue += 1;
+  return (counter.innerHTML = counterValue);
+}
+
+decrementBtn.addEventListener("click", Minus);
+incrementBtn.addEventListener("click", Plus);
