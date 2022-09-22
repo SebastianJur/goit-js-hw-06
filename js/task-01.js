3c05921b840e1820b95d648683efc1afae918d0a
Sebastian Jurewicz <senkartan@gmail.com>
2022-09-22 13:16:33 +0200

    Task 1 Done

--- js/task-01.js
+++ js/task-01.js
@@ -5,7 +5,7 @@ const categoriesCollection = ulList.querySelectorAll("li.item");
 console.log(`Number of categories: ${categoriesCollection.length}`);
 
 for (let categoriesCollectionItem of categoriesCollection) {
-  // TODO: try with getElementsByTagName
+  
   console.log(
     `Category: ${categoriesCollectionItem.querySelector("h2").innerText}`
   );
