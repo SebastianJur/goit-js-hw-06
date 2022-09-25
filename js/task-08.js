f7df45bb22baedff20fc2f01b3de487d28ad4cf6
Sebastian Jurewicz <senkartan@gmail.com>
2022-09-25 12:10:21 +0200

    Task 8 Done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector("form");
 
+const submitForm = (event) => {
+
+  event.preventDefault();
+
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (!email || !password) {
+    alert("Please fill in all the fields");
+  } else {
+    const userData = {
+      email: email,
+      password: password,
+    };
+
+    console.log(userData);
+
+    form.reset();
+  }
+};
+
+form.addEventListener("submit", submitForm);
